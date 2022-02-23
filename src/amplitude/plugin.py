af1f1cdc8a7a475b0007744bc93b8268a3682034
bohan-amplitude <bohan.li@amplitude.com>
2022-02-22 23:41:17 -0800

    timeline setup

--- src/amplitude/plugin.py
+++ src/amplitude/plugin.py
@@ -1,22 +1,31 @@
-class Plugin:
-
-    def __init__(self):
-        pass
+import abc
+from amplitude import constants
 
 
-class BeforePlugin(Plugin):
+class Plugin(abc.ABC):
 
-    def __init__(self):
-        pass
+    def __init__(self, plugin_type):
+        self.plugin_type = plugin_type
+        self.enable = True
 
-
-class EnrichmentPlugin(Plugin):
-
-    def __init__(self):
+    @abc.abstractmethod
+    def execute(self, event):
         pass
 
 
 class DestinationPlugin(Plugin):
 
-    def __init__(self):
+    def __init__(self, timeline):
+        super().__init__(constants.DESTINATION_PLUGIN_TYPE)
+        self.timeline = timeline
+        self.amplitude = timeline.amplitude
+        self.workers = self.amplitude.workers
+        self.storage = self.amplitude.storage
+        self.logger = self.amplitude.logger
+
+    def execute(self, event):
+        self.storage.push_to_buffer(event, 0)
+
+    def start(self):
         pass
+    
--- src/amplitude/timeline.py
+++ src/amplitude/timeline.py
@@ -1,23 +1,40 @@
-from amplitude.plugin import BeforePlugin, EnrichmentPlugin, DestinationPlugin
+import threading
+from amplitude import constants
 
 
 class Timeline:
 
-    def __init__(self):
-        self.before = []
-        self.enrichment = []
-        self.destination = []
+    def __init__(self, client):
+        self.locks = {
+            constants.BEFORE_PLUGIN_TYPE: threading.Lock(),
+            constants.ENRICHMENT_PLUGIN_TYPE: threading.Lock(),
+            constants.DESTINATION_PLUGIN_TYPE: threading.Lock()
+        }
+        self.plugins = {
+            constants.BEFORE_PLUGIN_TYPE: [],
+            constants.ENRICHMENT_PLUGIN_TYPE: [],
+            constants.DESTINATION_PLUGIN_TYPE: []
+        }
+        self.amplitude = client
 
     def add(self, plugin):
-        if isinstance(plugin, BeforePlugin):
-            self.before.append(plugin)
-        elif isinstance(plugin, EnrichmentPlugin):
-            self.enrichment.append(plugin)
-        elif isinstance(plugin, DestinationPlugin):
-            self.destination.append(plugin)
+        with self.locks[plugin.plugin_type]:
+            self.plugins[plugin.plugin_type].append(plugin)
 
     def remove(self, plugin):
-        pass
+        for plugin_type in self.locks:
+            with self.locks[plugin_type]:
+                self.plugins[plugin_type] = [p for p in self.plugins[plugin_type] if p != plugin]
 
     def process(self, event):
-        pass
+        before_result = self.apply_plugins(constants.BEFORE_PLUGIN_TYPE, event)
+        enrich_result = self.apply_plugins(constants.ENRICHMENT_PLUGIN_TYPE, before_result)
+        self.apply_plugins(constants.DESTINATION_PLUGIN_TYPE, enrich_result)
+        return enrich_result
+
+    def apply_plugins(self, plugin_type, event):
+        result = event
+        with self.locks[plugin_type]:
+            for plugin in self.plugins[plugin_type]:
+                result = plugin.execute(result)
+        return result
