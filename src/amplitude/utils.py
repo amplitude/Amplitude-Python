7ce5d72257804c4fd34933699713a6f20fc302a8
bohan-amplitude <bohan.li@amplitude.com>
2022-03-03 13:25:02 -0800

    combine if check

--- src/amplitude/utils.py
+++ src/amplitude/utils.py
@@ -2,10 +2,8 @@ from amplitude.event import BaseEvent
 
 
 def verify_event(event):
-    if not isinstance(event, BaseEvent):
-        return False
-    if not event["event_type"]:
-        return False
-    if not event["user_id"] and not event["device_id"]:
+    if (not isinstance(event, BaseEvent)) or \
+            (not event["event_type"]) or \
+            (not event["user_id"] and not event["device_id"]):
         return False
     return True
