c7f2628b5adc444d37d6deddf8dc403ffdd65dda
bohan-amplitude <bohan.li@amplitude.com>
2022-03-08 11:21:09 -0800

    resolve comments

--- src/amplitude/plugin.py
+++ src/amplitude/plugin.py
@@ -1,9 +1,11 @@
 import abc
 from enum import Enum
+from typing import Optional
+
 from amplitude.client import Amplitude
 from amplitude.storage import Storage
 from amplitude.timeline import Timeline
-from amplitude.event import BaseEvent
+from amplitude.event import BaseEvent, GroupIdentifyEvent, IdentifyEvent, RevenueEvent
 from amplitude import utils
 from amplitude.exception import InvalidEventError
 
@@ -12,44 +14,79 @@ class PluginType(Enum):
     BEFORE = 0
     ENRICHMENT = 1
     DESTINATION = 2
+    OBSERVE = 3
 
 
 class Plugin(abc.ABC):
 
     def __init__(self, plugin_type: PluginType):
         self.plugin_type: PluginType = plugin_type
-        self.enable = True
+        self.__amplitude_client = None
+
+    def setup(self, client: Amplitude):
+        self.__amplitude_client = client
 
     @abc.abstractmethod
-    def execute(self, event):
+    def execute(self, event: BaseEvent):
         pass
 
 
-class DestinationPlugin(Plugin):
+class EventPlugin(Plugin):
+
+    def __init__(self, plugin_type: PluginType):
+        super().__init__(plugin_type)
+
+    def execute(self, event: BaseEvent) -> Optional[BaseEvent]:
+        if isinstance(event, GroupIdentifyEvent):
+            return self.group_identify(event)
+        if isinstance(event, IdentifyEvent):
+            return self.identify(event)
+        if isinstance(event, RevenueEvent):
+            return self.revenue(event)
+        return self.track(event)
+
+    def group_identify(self, event: GroupIdentifyEvent) -> Optional[GroupIdentifyEvent]:
+        return event
+
+    def identify(self, event: IdentifyEvent) -> Optional[IdentifyEvent]:
+        return event
+
+    def revenue(self, event: RevenueEvent) -> Optional[RevenueEvent]:
+        return event
+
+    def track(self, event: BaseEvent) -> Optional[BaseEvent]:
+        return event
+
+
+class DestinationPlugin(EventPlugin):
 
     def __init__(self, storage: Storage = None, ):
         super().__init__(PluginType.DESTINATION)
-        self.timeline = Timeline()
-        self.amplitude = None
+        self.__timeline = Timeline()
+        self.__amplitude_client = None
         self.workers = None
         self.storage = storage
 
+    @property
+    def timeline(self):
+        return self.__timeline
+
     def setup(self, client: Amplitude):
-        self.amplitude = client
-        self.workers = self.amplitude.workers
+        self.__amplitude_client = client
+        self.__timeline.__amplitude_client = client
 
     def set_storage(self, storage: Storage):
         self.storage = storage
 
     def add(self, plugin):
-        self.timeline.add(plugin)
+        self.__timeline.add(plugin)
         return self
 
     def remove(self, plugin):
-        self.timeline.remove(plugin)
+        self.__timeline.remove(plugin)
         return self
 
-    def execute(self, event: BaseEvent):
+    def execute(self, event: BaseEvent) -> None:
         event = self.timeline.process(event)
         if not utils.verify_event(event):
             raise InvalidEventError("Invalid event.")
