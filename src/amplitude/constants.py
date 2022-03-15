bcb7518ce16e65b015689b0d9eaf439c0e5df167
bohan-amplitude <bohan.li@amplitude.com>
2022-03-15 11:02:37 -0700

    identify

--- src/amplitude/constants.py
+++ src/amplitude/constants.py
@@ -5,6 +5,18 @@ BATCH_API_URL_EU = "https://api.eu.amplitude.com/batch"
 
 IDENTIFY_EVENT = "$identify"
 GROUP_IDENTIFY_EVENT = "$groupidentify"
+IDENTITY_OP_ADD = "$add"
+IDENTITY_OP_APPEND = "$append"
+IDENTITY_OP_CLEAR_ALL = "$clearAll"
+IDENTITY_OP_PREPEND = "$prepend"
+IDENTITY_OP_SET = "$set"
+IDENTITY_OP_SET_ONCE = "$setOnce"
+IDENTITY_OP_UNSET = "$unset"
+IDENTITY_OP_PRE_INSERT = "$preInsert"
+IDENTITY_OP_POST_INSERT = "$postInsert"
+IDENTITY_OP_REMOVE = "$remove"
+UNSET_VALUE = "-"
+
 REVENUE_PRODUCT_ID = "$productId"
 REVENUE_QUANTITY = "$quantity"
 REVENUE_PRICE = "$price"
@@ -13,6 +25,7 @@ REVENUE_RECEIPT = "$receipt"
 REVENUE_RECEIPT_SIG = "$receiptSig"
 REVENUE = "$revenue"
 AMP_REVENUE_EVENT = "revenue_amount"
+
 MAX_PROPERTY_KEYS = 1024
 MAX_STRING_LENGTH = 1024
 
