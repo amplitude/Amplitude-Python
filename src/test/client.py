f32a8eb019e683aab17f4add43e05f9f4627ad28
bohan-amplitude <bohan.li@amplitude.com>
2022-03-25 09:56:08 -0700

    Update client.py

--- src/test/client.py
+++ src/test/client.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
+class AmplitudeClientTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
