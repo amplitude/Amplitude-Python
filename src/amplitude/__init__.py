f3eab0ab311e7cffa3d21f273ea7527e512950a6
bohan-amplitude <bohan.li@amplitude.com>
2022-03-30 15:03:25 -0700

    Update __init__.py

--- src/amplitude/__init__.py
+++ src/amplitude/__init__.py
@@ -1,3 +1,3 @@
 from amplitude.client import Amplitude
-from amplitude.event import BaseEvent, EventOptions, Identify, Revenue
+from amplitude.event import BaseEvent, EventOptions, Identify, Revenue, IdentifyEvent, GroupIdentifyEvent, RevenueEvent
 from amplitude.config import Config
