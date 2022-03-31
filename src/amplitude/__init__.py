571d6e25e4046fc16f78e448c6b9697203936e59
bohan-amplitude <bohan.li@amplitude.com>
2022-03-30 18:20:43 -0700

    pass copy of event to destination plugin
    put public available classes in package __init__.py and missing type hint

--- src/amplitude/__init__.py
+++ src/amplitude/__init__.py
@@ -1,3 +1,5 @@
 from amplitude.client import Amplitude
 from amplitude.event import BaseEvent, EventOptions, Identify, Revenue, IdentifyEvent, GroupIdentifyEvent, RevenueEvent
 from amplitude.config import Config
+from amplitude.constants import PluginType
+from amplitude.plugin import EventPlugin, DestinationPlugin
