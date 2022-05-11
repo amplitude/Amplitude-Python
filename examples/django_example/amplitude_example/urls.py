27bccaeb92cfb1b229ccf20be86b7004967839ea
bohan-amplitude <bohan.li@amplitude.com>
2022-05-10 17:37:38 -0700

    Update urls.py

--- examples/django_example/amplitude_example/urls.py
+++ examples/django_example/amplitude_example/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('pageloaded', views.track_page_load, name='Track Page Load Event'),
-    path('identify', views.set_age, name='Track Page Load Event'),
-    path('groupidentify', views.set_group_member, name='Track Page Load Event'),
-    path('setgroup', views.set_user_group, name='Track Page Load Event'),
-    path('revenue', views.track_revenue, name='Track Page Load Event'),
-    path('flush', views.flush_event, name='Track Page Load Event'),
+    path('identify', views.set_age, name='Identify Event'),
+    path('groupidentify', views.set_group_member, name='Group Identify Event'),
+    path('setgroup', views.set_user_group, name='Set User Group'),
+    path('revenue', views.track_revenue, name='Track Revenue Event'),
+    path('flush', views.flush_event, name='Flush Events'),
 ]
